ae12ee215f31ddf8c5a302b0fd893876eb931eb6
Mikhail Kalinin <m.kalinin@selectel.ru>
2018-07-30 16:39:41 +0300

    Add second layout for time

--- internal/itunes/utils.go
+++ internal/itunes/utils.go
@@ -2,12 +2,16 @@ package itunes
 
 import "time"
 
-const layout  =  "Jan 2, 2006"
+const layout1 = "Jan 2, 2006"
+const layout2 = "2 Jan 2006"
 
 func parseTime(value string) (*time.Time, error) {
-	t, err := time.Parse(layout, value)
+	t, err := time.Parse(layout1, value)
 	if err != nil {
-		return nil, err
+		t, err = time.Parse(layout2, value)
+		if err != nil {
+			return nil, err
+		}
 	}
 	return &t, nil
 }
--- internal/itunes/utils_test.go
+++ internal/itunes/utils_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestClient_Parse_Time(t *testing.T) {
+func TestClient_ParseTime_Layout1(t *testing.T) {
 	setup()
 	defer teardown()
 
@@ -18,4 +18,18 @@ func TestClient_Parse_Time(t *testing.T) {
 	assert.Equal(t, 18, res.Day())
 	assert.Equal(t, "July", res.Month().String())
 	assert.Equal(t, 2018, res.Year())
+}
+
+func TestClient_ParseTime_Layout2(t *testing.T) {
+	setup()
+	defer teardown()
+
+	// action
+	res, err := parseTime("10 Nov 2017")
+
+	// assert
+	assert.NoError(t, err)
+	assert.Equal(t, 10, res.Day())
+	assert.Equal(t, "November", res.Month().String())
+	assert.Equal(t, 2017, res.Year())
 }
