3fb813c54101b08d721d21e550ad54ed50533463
Mikhail Kalinin <m.kalinin@selectel.ru>
2018-08-10 15:21:35 +0300

    Add unsubscribe method into the db/subscriptions model

--- internal/db/subscriptions_test.go
+++ internal/db/subscriptions_test.go
@@ -54,3 +54,31 @@ func TestDB_Subscriptions_SubscribeUserForArtists(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Len(t, subs, 4)
 }
+
+func TestDB_Subscriptions_UnsubscribeUserFromArtists(t *testing.T) {
+	setup()
+	defer teardown()
+
+	// arrange
+	assert.NoError(t, DbMgr.EnsureSubscriptionExists(&Subscription{UserID: "objque@me", ArtistName: "Skrillex"}))
+	assert.NoError(t, DbMgr.EnsureSubscriptionExists(&Subscription{UserID: "objque@me", ArtistName: "Calvin Risk"}))
+	assert.NoError(t, DbMgr.EnsureSubscriptionExists(&Subscription{UserID: "objque@me", ArtistName: "AC/DC"}))
+	assert.NoError(t, DbMgr.EnsureSubscriptionExists(&Subscription{UserID: "mike@wels", ArtistName: "AC/DC"}))
+
+	// action
+	err := DbMgr.UnsubscribeUserFromArtists("objque@me", []string{"Calvin Risk"})
+
+	// assert
+	assert.NoError(t, err)
+
+	subs, err := DbMgr.FindAllUserSubscriptions("mike@wels")
+	assert.NoError(t, err)
+	assert.Len(t, subs, 1)
+	assert.Equal(t, "AC/DC", subs[0].ArtistName)
+
+	subs, err = DbMgr.FindAllUserSubscriptions("objque@me")
+	assert.NoError(t, err)
+	assert.Len(t, subs, 2)
+	assert.Equal(t, "AC/DC", subs[0].ArtistName)
+	assert.Equal(t, "Skrillex", subs[1].ArtistName)
+}
