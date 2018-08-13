55c9c9ea30b5c6055348d70d64afa5afd17a5c05
Mikhail Kalinin <m.kalinin@selectel.ru>
2018-08-13 18:53:15 +0300

    Fix error typo

--- internal/itunes/errors.go
+++ internal/itunes/errors.go
@@ -4,5 +4,5 @@ import "errors"
 
 var (
 	ArtistInactiveErr = errors.New("the last release from an artist was long ago or page is broken")
-	ArtistNotFoundErr = errors.New("artists not found")
+	ArtistNotFoundErr = errors.New("artist not found")
 )
