949a677be863fb7a2e7a917394a4defc8ffa24d9
Mikhail Kalinin <m.kalinin@selectel.ru>
2018-08-22 16:08:50 +0300

    Remove notification method from handler

--- internal/fetcher/handlers/itunes/store.go
+++ internal/fetcher/handlers/itunes/store.go
@@ -1,55 +1,24 @@
 package itunes
 
 import (
-	"github.com/jinzhu/gorm"
 	"github.com/objque/musicmash/internal/db"
-	"github.com/objque/musicmash/internal/fetcher/handlers"
 	"github.com/objque/musicmash/internal/log"
-	"github.com/objque/musicmash/internal/notify"
 	"github.com/pkg/errors"
 )
-
-const storeName = "itunes"
 
 type AppleMusicHandler struct{}
 
 func (h *AppleMusicHandler) GetStoreName() string {
-	return storeName
+	return "itunes"
 }
 
 func (h *AppleMusicHandler) Fetch(releases []*db.Release) {
 	for _, release := range releases {
 		log.Infof("Found a new info from '%s': '%d'", release.ArtistName, release.ID)
-		err := db.DbMgr.EnsureReleaseExistsInStore(storeName, string(release.ID), release.ID)
+		err := db.DbMgr.EnsureReleaseExistsInStore(h.GetStoreName(), string(release.ID), release.ID)
 		if err != nil {
-			log.Error(errors.Wrapf(err, "tried to save release in %s with id '%v'", storeName, release.ID))
+			log.Error(errors.Wrapf(err, "tried to save release in %s with id '%v'", h.GetStoreName(), release.ID))
 			continue
-		}
-	}
-}
-
-func (h *AppleMusicHandler) NotifySubscribers(releases []*handlers.ReleaseData) {
-	for _, release := range releases {
-		chats, err := db.DbMgr.GetAllChatsThatSubscribedFor(release.ArtistName)
-		if err != nil {
-			if err == gorm.ErrRecordNotFound {
-				log.Debugf("No one subscribed for '%s'", release.ArtistName)
-				continue
-			}
-
-			log.Error(err)
-			continue
-		}
-
-		for _, chat := range chats {
-			err := notify.Service.Send(map[string]interface{}{
-				"chatID":    chat.ID,
-				"releaseID": release.StoreID,
-				"store":     storeName,
-			})
-			if err != nil {
-				log.Error(errors.Wrapf(err, "tried to send message into telegram chat with id '%d'", chat.ID))
-			}
 		}
 	}
 }
--- internal/fetcher/handlers/store.go
+++ internal/fetcher/handlers/store.go
@@ -1,19 +1,10 @@
 package handlers
 
 import (
-	"time"
-
 	"github.com/objque/musicmash/internal/db"
 )
 
 type StoreHandler interface {
 	Fetch(releases []*db.Release)
-	NotifySubscribers(releases []*ReleaseData)
 	GetStoreName() string
-}
-
-type ReleaseData struct {
-	Date       time.Time
-	ArtistName string
-	StoreID    uint64
 }
