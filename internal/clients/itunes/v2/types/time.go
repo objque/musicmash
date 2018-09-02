27dfcb78f6eed6da5498b6237026370017831d3c
Mikhail Kalinin <m.kalinin@selectel.ru>
2018-09-02 17:01:45 +0300

    Support releases with only year in release-date

--- internal/clients/itunes/v2/types/time.go
+++ internal/clients/itunes/v2/types/time.go
@@ -16,5 +16,10 @@ func (t *Time) MarshalJSON() ([]byte, error) {
 func (t *Time) UnmarshalJSON(data []byte) error {
 	var err error
 	t.Value, err = time.ParseInLocation(strconv.Quote("2006-01-02"), string(data), time.FixedZone("UTC", 0))
+	if err != nil {
+		// some oldest releases has only year
+		// for example check albums for user: 331647880, 14064764, 101672291
+		t.Value, err = time.ParseInLocation(strconv.Quote("2006"), string(data), time.FixedZone("UTC", 0))
+	}
 	return err
 }
