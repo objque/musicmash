1f5cb9156a49597839ef7d3c1fd2ddbf6a494702
Mikhail Kalinin <m.kalinin@selectel.ru>
2018-09-17 19:13:42 +0300

    Add storeID into logging message about "not found album message"

--- internal/fetcher/v2/fetcher.go
+++ internal/fetcher/v2/fetcher.go
@@ -29,7 +29,7 @@ func (f *Fetcher) fetchWorker(id int, artists <-chan *db.Artist, releases chan<-
 		release, err := albums.GetLatestArtistAlbum(f.Provider, artist.StoreID)
 		if err != nil {
 			if err == albums.AlbumsNotFoundErr {
-				log.Debugf("Artist '%s' hasn't albums", artist.Name)
+				log.Debugf("Artist '%s' with id %d hasn't albums", artist.Name, artist.StoreID)
 				continue
 			}
 
