2300aaac3b0cf28d1611717bcfcafbbccb452bc8
Mikhail Kalinin <m.kalinin@selectel.ru>
2018-10-05 16:27:28 +0300

    Rewrite cron package

--- internal/cron/cron.go
+++ internal/cron/cron.go
@@ -4,11 +4,9 @@ import (
 	"time"
 
 	"github.com/jinzhu/gorm"
-	itunesProvider "github.com/objque/musicmash/internal/clients/itunes"
 	"github.com/objque/musicmash/internal/config"
 	"github.com/objque/musicmash/internal/db"
-	"github.com/objque/musicmash/internal/fetcher/handlers/itunes"
-	"github.com/objque/musicmash/internal/fetcher/v2"
+	"github.com/objque/musicmash/internal/fetcher"
 	"github.com/objque/musicmash/internal/log"
 )
 
@@ -31,23 +29,16 @@ func isMustFetch() bool {
 }
 
 func Run() {
-	provider := itunesProvider.NewProvider(config.Config.Store.URL, config.Config.Store.Token)
-	f := v2.Fetcher{Provider: provider}
-	f.RegisterHandler(&itunes.AppleMusicHandler{})
-	//f.RegisterHandler(yandex.New("https://music.yandex.ru", provider))
 	for {
 		if isMustFetch() {
 			now := time.Now().UTC()
 			log.Infof("Start fetching stage for '%s'...", now.String())
-			if err := f.FetchAndProcess(); err != nil {
-				log.Error(err)
-			} else {
-				// NOTE (m.kalinin): release.created_at must be after last_fetch, because
-				// notifier uses that time to find new releases;
-				log.Infof("Finish fetching stage '%s'...", time.Now().UTC().String())
-				db.DbMgr.SetLastActionDate(db.ActionFetch, now)
-			}
+			fetcher.Fetch()
+			log.Infof("Finish fetching stage '%s'...", time.Now().UTC().String())
 			log.Infof("Elapsed time '%s'", time.Now().UTC().Sub(now).String())
+			if err := db.DbMgr.SetLastActionDate(db.ActionFetch, now); err != nil {
+				log.Error("can't save last_fetch date")
+			}
 		}
 
 		time.Sleep(time.Hour)
