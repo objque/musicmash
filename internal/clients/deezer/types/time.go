827ea84d4e6ebc9c4d258def07e0458595972373
Mikhail Kalinin <objque@gmail.com>
2018-11-29 23:45:39 +0300

    Avoid deezer releases with empty date
    some releases have 0000-00-00 release date
    so we will recognise it as first day of first year and ignore it

--- internal/clients/deezer/types/time.go
+++ internal/clients/deezer/types/time.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"bytes"
 	"strconv"
 	"time"
 )
@@ -13,10 +14,21 @@ func (t *Time) MarshalJSON() ([]byte, error) {
 	return []byte(t.Value.Format(strconv.Quote("2006-01-02"))), nil
 }
 
+var emptyDate = []byte{34, 48, 48, 48, 48, 45, 48, 48, 45, 48, 48, 34} // 0000-00-00
+
 func (t *Time) UnmarshalJSON(data []byte) error {
 	var err error
 	t.Value, err = time.ParseInLocation(strconv.Quote("2006-01-02"), string(data), time.FixedZone("UTC", 0))
 	if err != nil {
+		// some deezer releases have 0000-00-00 release date
+		// e.g
+		// 'Paddy and the Rats' (id 470404)
+		// 'Michael Grimm' (id 567109)
+		if bytes.Equal(data, emptyDate) {
+			t.Value = time.Date(0001, time.January, 01, 00, 00, 00, 00, time.UTC)
+			return nil
+		}
+
 		// some oldest releases have year and month
 		// for example check albums for user: 14548963, 14589060
 		t.Value, err = time.ParseInLocation(strconv.Quote("2006-01"), string(data), time.FixedZone("UTC", 0))
