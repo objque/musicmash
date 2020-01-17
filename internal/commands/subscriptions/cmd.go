4cea34b9313c1c3215848fc6c620c5dc65a6e96d
Mikhail Kalinin <objque@gmail.com>
2020-01-17 10:29:26 +0300

    Add subs alias to subscriptions cmd

--- internal/commands/subscriptions/cmd.go
+++ internal/commands/subscriptions/cmd.go
@@ -4,9 +4,10 @@ import "github.com/spf13/cobra"
 
 func NewSubscriptionCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "subscription",
-		Short: "Manage subscriptions",
-		Args:  cobra.NoArgs,
+		Use:     "subscription",
+		Short:   "Manage subscriptions",
+		Aliases: []string{"subs"},
+		Args:    cobra.NoArgs,
 	}
 	cmd.AddCommand(
 		NewCreateCommand(),
