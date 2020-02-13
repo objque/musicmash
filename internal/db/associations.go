0347e0f04e05a74c978650a1497852c834f9cdda
Mikhail Kalinin <objque@gmail.com>
2020-02-14 00:00:24 +0300

    Add filter to db/associations model

--- internal/db/associations.go
+++ internal/db/associations.go
@@ -1,5 +1,7 @@
 package db
 
+import "github.com/jinzhu/gorm"
+
 type Association struct {
 	ID        int64  `json:"-"`
 	ArtistID  int64  `json:"artist_id"`
@@ -7,38 +9,58 @@ type Association struct {
 	StoreID   string `json:"id"`
 }
 
+type AssociationOpts struct {
+	ArtistID  int64
+	StoreName string
+	StoreID   string
+}
+
 type AssociationMgr interface {
 	GetAllAssociationsFromStore(name string) ([]*Association, error)
 	GetAssociationFromStore(artistID int64, store string) ([]*Association, error)
 	IsAssociationExists(storeName, storeID string) bool
 	EnsureAssociationExists(artistID int64, storeName, storeID string) error
+	FindAssociations(opts *AssociationOpts) ([]*Association, error)
 }
 
 func (mgr *AppDatabaseMgr) GetAssociationFromStore(artistID int64, store string) ([]*Association, error) {
-	association := []*Association{}
-	if err := mgr.db.Where("artist_id = ? and store_name = ?", artistID, store).Find(&association).Error; err != nil {
-		return nil, err
-	}
-	return association, nil
+	return mgr.FindAssociations(&AssociationOpts{ArtistID: artistID, StoreName: store})
 }
 
 func (mgr *AppDatabaseMgr) GetAllAssociationsFromStore(name string) ([]*Association, error) {
-	association := []*Association{}
-	if err := mgr.db.Where("store_name = ?", name).Find(&association).Error; err != nil {
-		return nil, err
-	}
-	return association, nil
+	return mgr.FindAssociations(&AssociationOpts{StoreName: name})
 }
 
 func (mgr *AppDatabaseMgr) IsAssociationExists(storeName, storeID string) bool {
-	association := Association{}
-	err := mgr.db.Where("store_name = ? and store_id = ?", storeName, storeID).First(&association).Error
-	return err == nil
+	associations, _ := mgr.FindAssociations(&AssociationOpts{StoreName: storeName, StoreID: storeID})
+	return len(associations) > 0
 }
 
 func (mgr *AppDatabaseMgr) EnsureAssociationExists(artistID int64, storeName, storeID string) error {
-	if !mgr.IsAssociationExists(storeName, storeID) {
-		return mgr.db.Create(&Association{ArtistID: artistID, StoreName: storeName, StoreID: storeID}).Error
+	return mgr.db.Create(&Association{ArtistID: artistID, StoreName: storeName, StoreID: storeID}).Error
+}
+
+func applyAssociationsFilters(db *gorm.DB, opts *AssociationOpts) *gorm.DB {
+	if opts.StoreID != "" {
+		db = db.Where("store_id = ?", opts.StoreID)
 	}
-	return nil
+	if opts.StoreName != "" {
+		db = db.Where("store_name = ?", opts.StoreName)
+	}
+	if opts.ArtistID != 0 {
+		db = db.Where("artist_id = ?", opts.ArtistID)
+	}
+	return db
+}
+
+func (mgr *AppDatabaseMgr) FindAssociations(opts *AssociationOpts) ([]*Association, error) {
+	association := []*Association{}
+	db := mgr.db
+	if opts != nil {
+		db = applyAssociationsFilters(db, opts)
+	}
+	if err := db.Find(&association).Error; err != nil {
+		return nil, err
+	}
+	return association, nil
 }
