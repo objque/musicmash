2d5d2c84d27599ebea461ea7a70be7eb41ba0fa5
Mikhail Kalinin <objque@gmail.com>
2020-05-26 00:13:00 +0300

    Refusal to use gorm: use sqlx for db/store

--- internal/db/stores.go
+++ internal/db/stores.go
@@ -1,7 +1,8 @@
 package db
 
 import (
-	"github.com/jinzhu/gorm"
+	"database/sql"
+
 	"github.com/musicmash/musicmash/internal/log"
 )
 
@@ -12,13 +13,16 @@ type Store struct {
 type StoreMgr interface {
 	IsStoreExists(name string) bool
 	EnsureStoreExists(name string) error
+	CreateStore(name string) (*Store, error)
 	GetAllStores() ([]*Store, error)
 }
 
 func (mgr *AppDatabaseMgr) IsStoreExists(name string) bool {
+	const query = "select * from stores where name = $1"
+
 	store := Store{}
-	if err := mgr.db.Where("name = ?", name).First(&store).Error; err != nil {
-		if gorm.IsRecordNotFoundError(err) {
+	if err := mgr.newdb.Get(&store, query, name); err != nil {
+		if err == sql.ErrNoRows {
 			return false
 		}
 
@@ -32,14 +36,27 @@ func (mgr *AppDatabaseMgr) EnsureStoreExists(name string) error {
 	if mgr.IsStoreExists(name) {
 		return nil
 	}
-	return mgr.db.Create(&Store{Name: name}).Error
+
+	_, err := mgr.CreateStore(name)
+	return err
+}
+
+func (mgr *AppDatabaseMgr) CreateStore(name string) (*Store, error) {
+	const query = "insert into stores (name) values ($1)"
+
+	_, err := mgr.newdb.Exec(query, name)
+
+	store := &Store{Name: name}
+	return store, err
 }
 
 func (mgr *AppDatabaseMgr) GetAllStores() ([]*Store, error) {
+	const query = "select * from stores"
+
 	stores := []*Store{}
-	err := mgr.db.Find(&stores).Error
-	if err != nil {
+	if err := mgr.newdb.Select(&stores, query); err != nil {
 		return nil, err
 	}
+
 	return stores, nil
 }
