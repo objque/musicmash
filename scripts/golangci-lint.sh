7039a9bd22431b1d28a94fa7fe2bbd087ada2a13
Mikhail Kalinin <objque@gmail.com>
2020-07-03 21:59:55 +0300

    Update github workflow: after install, tests and lints build image and push into gh and docker_hub

--- scripts/golangci-lint.sh
+++ scripts/golangci-lint.sh
@@ -2,6 +2,7 @@
 
 if ! which golangci-lint > /dev/null; then
     echo "==> Installing golangci-lint"
+    # do not forget to bump version in gh/action
     curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $GOPATH/bin v1.25.0
 fi
 
