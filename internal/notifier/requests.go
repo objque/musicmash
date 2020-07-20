9fd48d4b389d90320cc3c30d4ccc1630f575a4c9
Mikhail Kalinin <objque@gmail.com>
2020-07-20 17:33:13 +0300

    Drop out resp body from the notifications server if error occurred

--- internal/notifier/requests.go
+++ internal/notifier/requests.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
+	"github.com/musicmash/musicmash/internal/log"
 	"github.com/musicmash/musicmash/internal/version"
 )
 
@@ -29,8 +31,11 @@ func (n *Notifier) sendReleases(releases []*Notification) error {
 		return fmt.Errorf("tried to send request with notifications, but got err: %w", err)
 	}
 
-	_ = resp.Body.Close()
+	defer func() { _ = resp.Body.Close() }()
+
 	if resp.StatusCode != http.StatusOK {
+		b, _ = ioutil.ReadAll(resp.Body)
+		log.Warnf("error body from the notifications server: %v", string(b))
 		return fmt.Errorf("notifications server return status code %v, but expect 200", resp.StatusCode)
 	}
 
