aa087635e9f3113cccc3cade51758144169cc509
Mikhail Kalinin <objque@gmail.com>
2020-12-12 02:07:42 +0300

    Fix down init migration

--- migrations/20201211202422_init.down.sql
+++ migrations/20201211202422_init.down.sql
@@ -1,14 +1,15 @@
 BEGIN;
 
-DROP TABLE IF EXISTS "artist_details";
-DROP TABLE IF EXISTS "artist_headers";
-DROP TABLE IF EXISTS "artist_gallery";
-DROP TABLE IF EXISTS "artist_external_links";
-DROP TABLE IF EXISTS "artist_associations";
-DROP TABLE IF EXISTS "stores";
-DROP TABLE IF EXISTS "releases";
-DROP TABLE IF EXISTS "subscriptions";
-DROP TABLE IF EXISTS "artists";
-DROP TABLE IF EXISTS "last_actions";
+DROP TABLE IF EXISTS "artist_details" CASCADE;
+DROP TABLE IF EXISTS "artist_headers" CASCADE;
+DROP TABLE IF EXISTS "artist_gallery" CASCADE;
+DROP TABLE IF EXISTS "artist_posters" CASCADE;
+DROP TABLE IF EXISTS "artist_external_links" CASCADE;
+DROP TABLE IF EXISTS "artist_associations" CASCADE;
+DROP TABLE IF EXISTS "stores" CASCADE;
+DROP TABLE IF EXISTS "releases" CASCADE;
+DROP TABLE IF EXISTS "subscriptions" CASCADE;
+DROP TABLE IF EXISTS "artists" CASCADE;
+DROP TABLE IF EXISTS "last_actions" CASCADE;
 
 COMMIT;
